8cbc4bc96fcd97bc793e207387d2919c64ee822b
Anurag Verma <anurag.verma.ug20@nsut.ac.in>
2023-07-27 18:53:42 +0530

    fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -65,9 +65,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "99f58be5332fc2d73c15adfe8ee090c7",
-"/": "99f58be5332fc2d73c15adfe8ee090c7",
-"main.dart.js": "03cb58462792f1234849ab8892ddb095",
+"index.html": "45b3240dd4a972c5f6281adadb134086",
+"/": "45b3240dd4a972c5f6281adadb134086",
+"main.dart.js": "3a96f51a514293861a38ab08efdbaaac",
 "manifest.json": "f393956267731452763ed2f701d5444d",
 "version.json": "9f9cbb400ca9e5d449968d9b44c421d2"
 };
