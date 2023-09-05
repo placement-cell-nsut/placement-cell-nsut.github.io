b3c736a59ad7a4c6816ddfc088d4f7423f43ddd9
Anurag Verma <anurag.verma.ug20@nsut.ac.in>
2023-09-05 23:01:44 +0530

    favicon changes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -81,10 +81,11 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "bca4c5dfb67c0a5836ceb2073363651b",
-"/": "bca4c5dfb67c0a5836ceb2073363651b",
+"index.html": "1acfd24c969ae2cfdbbbe61db9e8674b",
+"/": "1acfd24c969ae2cfdbbbe61db9e8674b",
 "main.dart.js": "f1cb66915217af47e2693913bd540e18",
 "manifest.json": "f393956267731452763ed2f701d5444d",
+"tnplogo.png": "30987e6c5729974725591649d749308c",
 "version.json": "9f9cbb400ca9e5d449968d9b44c421d2"
 };
 
